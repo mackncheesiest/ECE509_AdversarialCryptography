74487adab661fdf157577d8f901227b3ba655f78
Cody Gunton <codygunton@gmail.com>
2017-04-30 14:38:30 -0700

    Implemented batched computation of L1 distances.

--- neighbors.py
+++ neighbors.py
@@ -8,38 +8,102 @@ encoder = Encoder()
 sess, trio = trained_trio('./modelcheckpoints/modelchkpt.chk')
 
 
-def neighbors(in_string):
-    # returns a list of strings that are nearest-neighbors of in_string
-
-    s = encoder.encode(in_string)
+def neighbors(in_bin):
+    # returns a list of strings that are nearest-neighbors of in_bin
+    # where in_bin is a binary string
+    s = in_bin
     neighbor_list = []
     for i in range(len(s)):
         l = list(s)
         l[i] = str((int(l[i]) + 1) % 2)
         neighbor_enc = ''.join(l)
-        neighbor_string = encoder.decode(neighbor_enc)
-        neighbor_list += [neighbor_string]
+        neighbor_list += [neighbor_enc]
     return neighbor_list
 
 
 def l1_distances(sess, trio, s, key):
-    # s is a string
-    # currently works with four-letter words (heh) because we haven't
-    # implemented a padding procedure
-
-    # returns a list of integers, one for each neighbor. the integer is
-    # the number of bits where the encoding of the ciphertext of the
-    # neighbor differs from the encoding of the ciphertext of s.
+    # compute l1 distances for a single plaintext
+    nearest_ns = [np.array(list(n), dtype='float32')
+                  for n in neighbors(encoder.encode(s))]
+    nearest_ns = np.concatenate([[n] for n in nearest_ns], axis=0)
+    keys = np.concatenate([key for i in range(20)], axis=0)
+    encrypted_ns = trio.encryptBatch(sess, nearest_ns, keys)
 
     c0 = trio.encryptPlaintext(sess, s, key)
     distances = []
-    for neighbor in neighbors(s):
-        c = trio.encryptPlaintext(sess, neighbor, key)
+    for c in encrypted_ns:
         distance = sum(np.abs(c - c0))
         distances += [distance]
 
     return distances
+# tester = l1_distances(sess, trio, 'test', generateData(20,1))
 
 
-key = generateData(20,1) #  NB: a new key with each run
-D = l1_distances(sess, trio, 'test',key)
+B = [bin(i)[2:].zfill(20) for i in range(2**20-128, 2**20)]
+B = [bin(i)[2:].zfill(20) for i in range(2**5)]
+
+
+def batch_neighbors(B):
+    # B is a list of binary strings
+    all_ns = np.array([])
+    L = []
+    for b in B:
+        L += [np.array(list(n), dtype='float32') for n in neighbors(b)]
+    all_ns = np.concatenate([[l] for l in L], axis=0)
+    return all_ns
+
+
+def batch_min_l1_distances(B, key):
+    # B is a list of binary strings
+    # key is an array, e.g., generateData(20,1)
+
+    # generate and encrypt neighbors
+    nearest_ns = batch_neighbors(B)
+    keys = np.concatenate([key for i in range(len(nearest_ns))], axis=0)
+    # 
+    encrypted_ns = trio.encryptBatch(sess, nearest_ns, keys)
+
+    # encrypt the strings in B
+    C = [np.array(list(b), dtype='float32') for b in B]
+    C = np.concatenate([[c] for c in C], axis=0)
+    keys = np.concatenate([key for i in range(len(C))], axis=0)
+    encrypted_bs = trio.encryptBatch(sess, C, keys)
+
+    min_distances = []
+    for c0 in encrypted_bs:
+        distances = []
+        for c in encrypted_ns[:20]:
+            distance = sum(np.abs(c - c0))
+            distances += [distance]
+            min_distances += [min(distances)]
+        encrypted_bs = encrypted_bs[20:]
+
+    return min_distances
+
+def batch_extreme_l1_distances(B, key):
+    # B is a list of binary strings
+    # key is an array, e.g., generateData(20,1)
+
+    # generate and encrypt neighbors
+    nearest_ns = batch_neighbors(B)
+    keys = np.concatenate([key for i in range(len(nearest_ns))], axis=0)
+    encrypted_ns = trio.encryptBatch(sess, nearest_ns, keys)
+
+    # encrypt the strings in B
+    C = [np.array(list(b), dtype='float32') for b in B]
+    C = np.concatenate([[c] for c in C], axis=0)
+    keys = np.concatenate([key for i in range(len(C))], axis=0)
+    encrypted_bs = trio.encryptBatch(sess, C, keys)
+
+    min_distances = []
+    max_distances = []
+    for c0 in encrypted_bs:
+        distances = []
+        for c in encrypted_ns[:20]:
+            distance = sum(np.abs(c - c0))
+            distances += [distance]
+            min_distances += [min(distances)]
+            max_distances += [max(distances)]
+        encrypted_bs = encrypted_bs[20:]
+
+    return list(zip(min_distances, max_distances))
