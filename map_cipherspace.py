e9f5cc90815fb6cf7db4c7369c07667eb6f56406
mackncheesiest <mackncheesiest@gmail.com>
2017-04-30 22:44:14 -0700

    First go at using batched nearest neighbors seemed to work well.......
    Would like some feedback on whether or not it looks to be doing everything
    *correctly* though. Update to #5 since I think min and max NN distances
    will be more useful anyway.

--- map_cipherspace.py
+++ map_cipherspace.py
@@ -1,7 +1,7 @@
 from encoder import Encoder
 from nets import generateData
 from loader import trained_trio
-from neighbors import l1_distances
+from neighbors import batch_extreme_l1_distances
 import numpy as np
 
 
@@ -21,12 +21,10 @@ k = generateData(plaintext_len=20, batch_size=1)
 keys = k * np.ones([2**12, 20])
 
 # Each element is like 4 bytes, so...only like 80 MB of memory? Not terrible
+"""
 myCiphertexts = np.zeros([2**20, 20])
 
-# There are 2**20 different plaintexts, each of them has 20 nearest neighbors
-myNNs = np.zeros([2**20, 20])
 
-"""
 print("calculating all possible ciphertexts with a random key...")
 # 256 total batches of 4096 each gives 256 * 4096 = 1048576 = 2^20 combinations
 for i in range(2**8):
@@ -45,13 +43,25 @@ print("writing ciphertexts to file...")
 np.savetxt(ciphertexts_filename, myCiphertexts)
 print("finished writing ciphertexts to file...")
 """
+
+# There are 2**20 different plaintexts, each of them has 20 nearest neighbors, 
+# of which we are collecting the min and max as a tuple (one element)
+myNNs = np.zeros([2**20, 2])
+
 print("calculating nearest neighbors...")
 # Next, calculate all the nearest neighbors
+# Use a fixed random key
 k = generateData(plaintext_len=20, batch_size=1)
-for i in range(2**20):
-    myNNs[i, :] = l1_distances(sess, trio, myEnc.decode(format(i, 'b').zfill(20)), k)
-    if i % 2**10 == 0:
-        print(str(i/2**20 * 100) + "% done...")
+for i in range(2**8):
+    # Batch size of 4096
+    tempArr = np.zeros([2**12, 20])
+    for j in range(2**12):
+        tempArr[j, :] = [np.float32(x) for x in format(j + 4096*i, 'b').zfill(20)]
+    
+    # Get the array of extreme nearest-neighbor distances for this batch
+    myNNs[(4096*i):(4096*(i+1)), :] = batch_extreme_l1_distances(sess, trio, tempArr, k)
+    if i % 2**4 == 0:
+        print(str(4096*i/(2**20) * 100) + "% done...")
 
 # And write this big numpy array to a file
 print("writing nearest neighbors to file...")
